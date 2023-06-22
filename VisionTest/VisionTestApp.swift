5d768c59e8764433795bb6d498b93ee01f48ee1d
Fadi Sayfi <fsayfi@icloud.com>
2023-06-21 22:15:15 -0400

    Initial Commit

---
+++ VisionTest/VisionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTestApp.swift
+//  VisionTest
+//
+//  Created by Fadi Sayfi on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
